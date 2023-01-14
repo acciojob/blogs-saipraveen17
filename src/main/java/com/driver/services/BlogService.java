22d6bffc7a7ec05dc92feed2d61aab22d8aa7b5f
Praveen <praveenk9417@gmail.com>
2023-01-14 19:37:45 +0530

    Submission

--- src/main/java/com/driver/services/BlogService.java
+++ src/main/java/com/driver/services/BlogService.java
@@ -6,6 +6,7 @@ import com.driver.models.User;
 import com.driver.repositories.BlogRepository;
 import com.driver.repositories.ImageRepository;
 import com.driver.repositories.UserRepository;
+import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -26,27 +27,44 @@ public class BlogService {
 
     public List<Blog> showBlogs(){
         //find all blogs
-
+        return blogRepository1.findAll();
     }
 
     public void createAndReturnBlog(Integer userId, String title, String content) {
         //create a blog at the current time
-
+        Date date = new Date();
+        Blog blog = new Blog();
+        User user = userRepository1.findById(userId).get();
         //updating the blog details
-
+        blog.setContent(content);
+        blog.setTitle(title);
+        blog.setPubDate(date);
+        blog.setUser(user);
         //Updating the userInformation and changing its blogs
+        user.getBlogList().add(blog);
+        userRepository1.save(user);
 
     }
 
     public Blog findBlogById(int blogId){
         //find a blog
+        Blog blog = blogRepository1.findById(blogId).get();
+        return blog;
     }
 
     public void addImage(Integer blogId, String description, String dimensions){
         //add an image to the blog after creating it
+        Blog blog = findBlogById(blogId);
+        Image image = imageService1.createAndReturn(blog, description, dimensions);
     }
 
     public void deleteBlog(int blogId){
         //delete blog and corresponding images
+        Blog blog = findBlogById(blogId);
+        User user = blog.getUser();
+        blogRepository1.deleteById(blogId);
+        user.getBlogList().remove(blog);
+        userRepository1.save(user);
+
     }
 }
--- src/main/java/com/driver/services/ImageService.java
+++ src/main/java/com/driver/services/ImageService.java
@@ -1,6 +1,7 @@
 package com.driver.services;
 
 import com.driver.models.*;
+import com.driver.repositories.BlogRepository;
 import com.driver.repositories.ImageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,22 +10,66 @@ import org.springframework.stereotype.Service;
 public class ImageService {
     @Autowired
     ImageRepository imageRepository2;
+    @Autowired
+    private BlogRepository blogRepository1;
 
     public Image createAndReturn(Blog blog, String description, String dimensions){
         //create an image based on given parameters and add it to the imageList of given blog
+        Image image = new Image(description, dimensions);
+        blog.getImageList().add(image);
+        blogRepository1.save(blog);
+        return image;
     }
 
     public void deleteImage(Image image){
 
+        //Blog blog = image.getBlog();
+        imageRepository2.delete(image);
+//        blog.getImageList().remove(image);
+//        blogRepository.save(blog);
     }
 
     public Image findById(int id) {
 
+        return imageRepository2.findById(id).get();
     }
 
     public int countImagesInScreen(Image image, String screenDimensions) {
         //Find the number of images of given dimensions that can fit in a screen having `screenDimensions`
         //In case the image is null, return 0
+        String imageDimensions = image.getDimensions();
+        int countImages = 0;
+        int imageLength, imageBreadth, screenLength, screenBreadth;
+        int[] imageDim = getDimensions(imageDimensions);
+        int[] screenDim = getDimensions(screenDimensions);
+        imageLength = imageDim[0];
+        imageBreadth = imageDim[1];
+        screenLength = screenDim[0];
+        screenBreadth = screenDim[1];
+        int lenCount = screenLength/imageLength;
+        int bdtCount = screenBreadth/imageBreadth;
+        countImages = lenCount*bdtCount;
+        return countImages;
+    }
+    public int[] getDimensions(String dimension) {
 
+        int imageLength, imageBreadth;
+        StringBuilder sb = new StringBuilder();
+        int i;
+        for(i=0; i<dimension.length(); i++) {
+            if(Character.isDigit(dimension.charAt(i))) {
+                sb.append(dimension.charAt(i));
+            }
+            else break;
+        }
+        imageLength = Integer.valueOf(sb.toString());
+        sb = new StringBuilder();
+        for(;i<dimension.length(); i++) {
+            if(Character.isDigit(dimension.charAt(i))) {
+                sb.append(dimension.charAt(i));
+            }
+        }
+        imageBreadth = Integer.valueOf(sb.toString());
+        return new int[]{imageLength, imageBreadth};
     }
 }
