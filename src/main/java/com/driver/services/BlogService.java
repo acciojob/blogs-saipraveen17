060003e11a936080b20a41ec8a3af3c507d566fa
Praveen <praveenk9417@gmail.com>
2023-01-14 20:42:27 +0530

    Submission 5

--- src/main/java/com/driver/services/BlogService.java
+++ src/main/java/com/driver/services/BlogService.java
@@ -56,6 +56,12 @@ public class BlogService {
         //add an image to the blog after creating it
         Blog blog = findBlogById(blogId);
         Image image = imageService1.createAndReturn(blog, description, dimensions);
+        image.setBlog(blog);
+        List<Image> imageList = blog.getImageList();
+        if(imageList==null) imageList = new ArrayList<>();
+        imageList.add(image);
+        blog.setImageList(imageList);
+        blogRepository1.save(blog);
     }
 
     public void deleteBlog(int blogId){
