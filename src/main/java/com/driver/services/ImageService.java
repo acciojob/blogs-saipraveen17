10406372bb8db1b8ea4981ff69e5c81fdfa587d4
Praveen <praveenk9417@gmail.com>
2023-01-14 20:37:20 +0530

    Submission 4

--- src/main/java/com/driver/services/ImageService.java
+++ src/main/java/com/driver/services/ImageService.java
@@ -19,9 +19,11 @@ public class ImageService {
     public Image createAndReturn(Blog blog, String description, String dimensions){
         //create an image based on given parameters and add it to the imageList of given blog
         Image image = new Image(description, dimensions);
+        image.setBlog(blog);
         List<Image> imageList = blog.getImageList();
         if(imageList==null) imageList = new ArrayList<>();
         imageList.add(image);
+        blog.setImageList(imageList);
         blogRepository1.save(blog);
         return image;
     }
